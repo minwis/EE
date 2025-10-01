2493f5ffbac3384d2e919a944b33c96b29488b8f
AhnMinsur <93420148+minwis@users.noreply.github.com>
2025-10-02 00:00:02 +0530

    final

--- src/MAIN.java
+++ src/MAIN.java
@@ -3,7 +3,10 @@ package uk.ac.ebi.uniprot.dataservice.client.examples;
 import uk.ac.ebi.kraken.interfaces.uniprot.UniProtEntry;
 import uk.ac.ebi.kraken.model.factories.DefaultUniProtFactory;
 import uk.ac.ebi.kraken.parser.UniProtParser;
+import uk.ac.ebi.uniprot.dataservice.client.Client;
+import uk.ac.ebi.uniprot.dataservice.client.ServiceFactory;
 import uk.ac.ebi.uniprot.dataservice.client.exception.ServiceException;
+import uk.ac.ebi.uniprot.dataservice.client.uniprot.UniProtService;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,95 +18,23 @@ import static uk.ac.ebi.uniprot.dataservice.client.examples.SmithWatermanStriped
 
 public class MAIN {
 
-    // load from file (***)
-    public static String getSequenceFromFile(String filename) {
-        File f = new File(filename);
-        System.out.println("Input Absolute path: " + f.getAbsolutePath());
-        System.out.println("File size in bytes: " + f.length());
-        try (FileInputStream fis = new FileInputStream(f)) {
-            UniProtEntry eD = UniProtParser.parse(fis, DefaultUniProtFactory.getInstance(), true);
-            return eD.getSequence().getValue();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
-
     public static void main(String[] args) throws ServiceException {
 
-        long timeForStripped = 0;
-        long timeForOriginal = 0;
-/*
         ServiceFactory serviceFactoryInstance = Client.getServiceFactoryInstance();
         UniProtService uniProtService = serviceFactoryInstance.getUniProtQueryService();
 
-       String targetProteinName = "P10415"; //A0A1B0GTW7
+        String targetProteinName = "P10415"; //A0A1B0GTW7
         String D = uniProtService.getEntry(targetProteinName).getSequence().getValue();
         String querySequenceName = "P49950"; //A0A1L8HYT7
         String Q = uniProtService.getEntry(querySequenceName).getSequence().getValue();
-*/
-        // load from file (***)
-        //String D = getSequenceFromFile("P10415.txt");
-        //String Q = getSequenceFromFile("P49950.txt");
-
-        String D = "TGTTACGG";
-        String Q = "GGTTGACTA";
 
         int lenD = D.length();
         int lenQ = Q.length();
         System.out.println("Size: " + lenD * lenQ + ", D : " + lenD + ", Q : " + lenQ);
 
+
         System.out.println("Original Time(ns): " + Original(D, lenD, Q, lenQ));
         System.out.println("Stripped-Layout Time(ns): " + StripedLayout(D, lenD, Q, lenQ));
-
-        /*
-        String targetProteinName = "Q8WZ42-1";
-        String D = uniProtService.getEntry(targetProteinName).getSequence().getValue();
-        int lenD = D.length();
-
-        Query query = UniProtQueryBuilder.proteinName("Titin");
-        QueryResult<UniProtEntry> entries = uniProtService.getEntries(query);
-
-
-
-        for ( int i = 0; i < 5; i++ )  { //(entries.hasNext()) Vector ==> arr/list ==> scalar
-
-            UniProtEntry entry = entries.next();
-            AlternativeProductsComment altProdComment = null;
-            for (Comment comment : entry.getComments()) {
-                if (comment.getCommentType() == CommentType.ALTERNATIVE_PRODUCTS) {
-                    altProdComment = (AlternativeProductsComment) comment;
-                    break;
-                }
-            }
-
-            String Q = uniProtService.getEntry(String.valueOf(entry.getUniProtId())).getSequence().getValue();
-            int lenQ = Q.length();
-
-            timeForStripped+=StripedLayout(D, lenD, Q, lenQ);
-            timeForOriginal+=Original(D, lenD, Q, lenQ);
-
-            if ( altProdComment != null ) {
-                List<AlternativeProductsIsoform> isoforms = altProdComment.getIsoforms();
-                if ( !isoforms.isEmpty() ) {
-                    for ( int j = 0; j < isoforms.toArray().length; j++ ) {
-                        Q = uniProtService.getEntry(isoforms.get(i).getIds().getFirst().getValue()).getSequence().getValue();
-                        lenQ = Q.length();
-
-                        timeForStripped+=StripedLayout(D, lenD, Q, lenQ);
-                        timeForOriginal+=Original(D, lenD, Q, lenQ);
-                    }
-                }
-            }
-
-        }
-
-
-        for ( int i = 0; i < 4; i++ ) {
-            System.out.println("Stripped Layout: " + timeForStripped + "\nOriginal: " + timeForOriginal );
-        }
-
-        */
 
 
     }
--- src/SmithWatermanStripedLayout.java
+++ src/SmithWatermanStripedLayout.java
@@ -7,11 +7,11 @@ import java.util.Arrays;
 public class SmithWatermanStripedLayout {
 
     static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_256;
-    static int segLen = FloatVector.SPECIES_256.length(); //number of vectors
+    static int segLen = SPECIES.length();
 
     static int segN;
 
-    public static long StripedLayout( String D, int lenD, String Q, int lenQ ) {
+    public static long StripedLayout( String T, int lenT, String Q, int lenQ ) {
         long startTime = System.nanoTime();
 
         segN = (lenQ + segLen - 1) / segLen;
@@ -51,20 +51,20 @@ public class SmithWatermanStripedLayout {
 
         float[] allZeroes = new float[SPECIES.length()];
 
-        FloatVector[][] vProfile = new FloatVector[lenD][segN];
-        for ( int i = 0; i < lenD; i++ ) {
-            int residueD = D.charAt(i);
-            FloatVector vResidueD = FloatVector.broadcast(SPECIES, residueD);
+        FloatVector[][] vProfile = new FloatVector[lenT][segN];
+        for ( int i = 0; i < lenT; i++ ) {
+            int residueT = T.charAt(i);
+            FloatVector vResidueT = FloatVector.broadcast(SPECIES, residueT);
             for ( int j = 0; j < segN; j++ ) {
                 int qSegmentStartIndex = j * segLen;
                 FloatVector vResidueQ = FloatVector.fromArray(SPECIES, floatArrayQ, qSegmentStartIndex);
-                VectorMask<Float> residueComparisonMask = vResidueD.compare(VectorOperators.EQ, vResidueQ);
+                VectorMask<Float> residueComparisonMask = vResidueT.compare(VectorOperators.EQ, vResidueQ);
                 VectorMask<Float> withinRange = SPECIES.indexInRange(0, (lenQ-j + segN-1)/segN);
                 vProfile[i][j] = FloatVector.fromArray(SPECIES, allZeroes,0).blend((vUnmatch.blend(vMatch, residueComparisonMask)), withinRange);
             }
         }
 
-        FloatVector[][] vHResult = new FloatVector[lenD][segN];
+        FloatVector[][] vHResult = new FloatVector[lenT][segN];
         FloatVector[] vHStore = new FloatVector[segN];
         FloatVector[] vHLoad = new FloatVector[segN];
         FloatVector[] vE = new FloatVector[segN];
@@ -80,66 +80,49 @@ public class SmithWatermanStripedLayout {
         vZero = FloatVector.fromArray(SPECIES, allZeroes, 0);
         vMax = FloatVector.fromArray(SPECIES, allZeroes, 0);
 
-        // Outer loop to process the database sequence
-        // for i:=0...dbLen
-        for ( int i = 0; i < lenD; i++ ) {
-            // Initialize F value to zeros. Any errors to vH values will be corrected in the Lazy-F loop.
+        for ( int i = 0; i < lenT; i++ ) {
             vF = FloatVector.fromArray(SPECIES, allZeroes,0);
 
-            // Adjust the last H value to be used in the next segment over
             withinRange = SPECIES.indexInRange(1, (lenQ-0 + segN-1)/segN );
             vH = vHStore[segN - 1].unslice(1, vZero, 0, withinRange);
 
-            // Swap the two H buffers - change to store all
             vHLoad = vHStore;
             vHStore = vHResult[i];
 
-            // Inner loop to process the query sequence
             for (int j = 0; j < segN; j++) {
-                // Add the scoring profile to vH
                 withinRange = SPECIES.indexInRange(0, (lenQ-j + segN-1)/segN );
-                vH = vH.add(vProfile[i][j], withinRange); // Add score from scoring profile
+                vH = vH.add(vProfile[i][j], withinRange);
 
-                // Save the vH values greater than the max
                 vMax = vMax.max(vH);
 
-                // Adjust vH with any greater vE or vH values
                 vH = vH.max(vE[j]);
                 vH = vH.max(vF);
 
-                // Save the vH values off
-                vHStore[j] = vH.max(vZero); // compare 0 and copy
+                vHStore[j] = vH.max(vZero);
 
-                // Calculate the new vE and vF based on the gap penalties for this search
                 vH = vH.add(vGapO, withinRange);
                 vE[j] = vE[j].add(vGapE, withinRange);
                 vE[j] = vE[j].max(vH);
                 vF = vF.add(vGapE, withinRange);
                 vF = vF.max(vH);
 
-                // Load next vH
                 vH = vHLoad[j];
             }
 
-            // --- Lazy-F loop ---
-            // Shift the vF left so its values can be used to correct the next segment over
-            // probably wrong in paper vF := VF >> 1;
             withinRange = SPECIES.indexInRange(1, (lenQ-0 + segN-1)/segN );
             vF = vF.unslice(1, vZero, 0, withinRange);
 
-            // Correct the vH values until there are no elements in vF that could influence the vH values
             int j = 0;
             withinRange = SPECIES.indexInRange(0, (lenQ-j + segN-1)/segN );
             VectorMask<Float> vCompare = vF.compare(VectorOperators.GT, vHStore[j], withinRange);
             while(vCompare.anyTrue()) {
                 vHStore[j] = vHStore[j].max(vF);
-                vMax = vMax.max(vHStore[j]); // probably missed in paper
+                vMax = vMax.max(vHStore[j]);
                 vH = vHStore[j].add(vGapO, withinRange);
-                vE[j] = vE[j].max(vH); // probably missed in paper
+                vE[j] = vE[j].max(vH);
                 vF = vF.add(vGapE, withinRange);
-                vF = vF.max(vH); // probably missed in paper
+                vF = vF.max(vH);
                 if (++j >= segN) {
-                    // If we processed the entire segment, we need to carry the vF values to the next segment
                     withinRange = SPECIES.indexInRange(1, (lenQ-0 + segN-1)/segN );
                     vF = vF.unslice(1, vZero, 0, withinRange);
                     j = 0;
@@ -152,7 +135,6 @@ public class SmithWatermanStripedLayout {
 
         long endTime = System.nanoTime();
         System.out.println("Stripped-Layout max " + vMax.reduceLanes(VectorOperators.MAX));
-        //System.out.println("FloatVector[] Left Shift time: " + maxTime);
         return (long) endTime - startTime;
     }
 }
